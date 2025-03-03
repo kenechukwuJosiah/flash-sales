c192b306af7dae366144e64618a0c0a1bade5ac2
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2025-03-03 20:30:57 +0100

    update: added joi validator for inboud payloads

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { User } from '../models';
 import jwt from 'jsonwebtoken';
+import { loginValidator, userValidator } from '../validators';
 
 
 // Generate a JWT token with the user's id and a secret key
@@ -18,12 +19,18 @@ const generateToken = (id: string) => {
  */
 export const registerUser = async (req: Request, res: Response) => {
   try {
+    const { error } = userValidator.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ success: false, message: error.details[0].message });
+    }
+
     const { name, email, password } = req.body;
 
 
     const userExists = await User.findOne({ email });
     if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ success: false, message: 'User already exists' });
     }
 
     const user = await User.create({
@@ -34,16 +41,20 @@ export const registerUser = async (req: Request, res: Response) => {
 
     if (user) {
       res.status(201).json({
+        success: true,
+        message: 'User created successfully',
+        data: {
         _id: user._id,
         name: user.name,
         email: user.email,
         token: generateToken(user._id),
+      }
       });
     } else {
-      res.status(400).json({ message: 'Unable to create user' });
+      res.status(400).json({ success: false, message: 'Unable to create user' });
     }
   } catch (error: any) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ success: false, message: error.message });
   }
 };
 
@@ -58,19 +69,31 @@ export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
+    const { error } = loginValidator.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ success: false, message: error.details[0].message });
+    }
+
+
     const user = await User.findOne({ email }).select('+password');
 
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ success: false, message: 'Invalid email or password' });
     }
 
+    
     res.status(200).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
+      success: true,
+      message: 'Login successful',
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      }
     });
   } catch (error: any) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ success: false, message: error.message });
   }
 }
